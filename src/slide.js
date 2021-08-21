c211266cff1eee2913b467740d237860d458cee0
imonaar <kevinmonari22@gmail.com>
2021-08-21 11:01:54 +0300

    final touches

--- src/slide.js
+++ src/slide.js
@@ -1,47 +1,38 @@
-const track = document.querySelector('.carousel__track')
-const slides = Array.from(track.children)
-const prevBtns = document.querySelectorAll('.btn-prev')
-const nextBtns = document.querySelectorAll('.btn-next')
+// ignore this script. for my own documentation
+
+// const track = document.querySelector('.carousel__track')
+// const slides = Array.from(track.children)
+// const prevBtn = document.querySelector('.btn-prev')
+// const nextBtn = document.querySelector('.btn-next')
 
 
-const slideWidth = slides[0].getBoundingClientRect().width
+// const slideWidth = slides[0].getBoundingClientRect().width
 
 
-const setSlidePosition = (slide, indx) =>{
-    slide.style.left = (slideWidth * indx )+ 'px'
-}
+// const setSlidePosition = (slide, indx) =>{
+//     slide.style.left = (slideWidth * indx )+ 'px'
+// }
 
-slides.forEach(setSlidePosition)
+// slides.forEach(setSlidePosition)
 
-const moveToSlide = (track, currentSlide, targetSlide) =>{
-    track.style.transform = 'translateX(-' + targetSlide.style.left +')'
-    currentSlide.classList.remove('current-slide')
-    targetSlide.classList.add('current-slide')
-}
+// const moveToSlide = (track, currentSlide, targetSlide) =>{
+//     track.style.transform = 'translateX(-' + targetSlide.style.left +')'
+//     currentSlide.classList.remove('current-slide')
+//     targetSlide.classList.add('current-slide')
+// }
 
-nextBtns.forEach((btn, indx)=>{
-    btn.addEventListener('click', e =>{
-        const currentSlide = track.querySelector('.current-slide')
-        const nextSlide = currentSlide.nextElementSibling
-    
-        //move to the next slide 
-    
-        moveToSlide(track, currentSlide, nextSlide)
-    })
-})
-
-prevBtn.addEventListener('click', e =>{
-    const currentSlide = track.querySelector('.current-slide')
-    const prevSlide = currentSlide.previousElementSibling
+// prevBtn.addEventListener('click', e =>{
+//     const currentSlide = track.querySelector('.current-slide')
+//     const prevSlide = currentSlide.previousElementSibling
    
-    moveToSlide(track, currentSlide, prevSlide)
-})
+//     moveToSlide(track, currentSlide, prevSlide)
+// })
 
-nextBtn.addEventListener('click', e =>{
-    const currentSlide = track.querySelector('.current-slide')
-    const nextSlide = currentSlide.nextElementSibling
+// nextBtn.addEventListener('click', e =>{
+//     const currentSlide = track.querySelector('.current-slide')
+//     const nextSlide = currentSlide.nextElementSibling
 
-    //move to the next slide 
+//     //move to the next slide 
 
-    moveToSlide(track, currentSlide, nextSlide)
-})
+//     moveToSlide(track, currentSlide, nextSlide)
+// })
